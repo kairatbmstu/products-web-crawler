5e193672b46c9b84e7f02b1a40b6e7f51e5aca6e
Kairat <kairat_y@mail.ru>
2022-08-20 12:55:42 +0600

    some changes;

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,10 @@
 #include <thread>
 #include "service/service.h"
 
-using WebCrawler::Service::RunSeeder;
-
-void runner(){
-    RunSeeder();
-}
-
 int main(int argc, char **argv)
 {
     // HTTP
     httplib::Server svr;
-
-    std::thread t(runner);
 
     svr.Get("/hi", [](const httplib::Request &, httplib::Response &res)
             { res.set_content("Hello World!", "text/plain"); });
